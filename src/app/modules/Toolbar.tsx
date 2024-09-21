3844ebd801102c3a04e137d554c450314d793559
Carlos Cobas <carloscobas@Carloss-MacBook-Pro.local>
2024-09-21 14:22:19 +0200

    Sistema de filtrado de modalidades

--- src/app/modules/Toolbar.tsx
+++ src/app/modules/Toolbar.tsx
@@ -2,7 +2,7 @@ import { ChangeEventHandler } from 'react';
 import ModalitySelector from './ModalitySelector';
 import RootNoteSelector from './RootNoteSelector'
 
-export default function Toolbar({onChangeRootNote, onChangeModality} : {onChangeRootNote:ChangeEventHandler<HTMLSelectElement>, onChangeModality: any}) {
+export default function Toolbar({onChangeRootNote, onChangeModality} : {onChangeRootNote:ChangeEventHandler<HTMLSelectElement>, onChangeModality: ChangeEventHandler<HTMLSelectElement>}) {
 
     return (
         <section className="toolbar-holder">
